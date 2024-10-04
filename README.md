629f287bde645afb87653233d05877dfa07194d1
Théophile Molcard <molcard.t@gmail.com>
2024-10-04 11:50:15 +0200

    Initial commit (#1)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# hackaton_mobilite
+# hackathon_mobilite
+
+To launch on idfm Onyxia plateform follow this [link](https://datalab.data-platform-self-service.net/launcher/ide/jupyter-python?autoLaunch=true&onyxia.friendlyName=%C2%ABpython-rag%C2%BB&init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com%2FIleDeFranceMobilites%2Fhackathon_mobilite%2Frefs%2Fheads%2Fmain%2Fscripts%2Finit_jupyter.sh)
---
+++ scripts/init_jupyter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+WORK_DIR="/home/onyxia/work"
+CLONE_DIR="${WORK_DIR}/git_hackaton_llm_onyxia"
+
+# Clone course repository
+REPO_URL="https://github.com/IleDeFranceMobilites/hackathon_mobilite"
+git clone --depth 1 $REPO_URL $CLONE_DIR
+
+# Put relevant notebook in formation dir
+cp "${CLONE_DIR}/notebooks/vector_db_template.ipynb" "${WORK_DIR}"
+cp "${CLONE_DIR}/notebooks/rag_template.ipynb" "${WORK_DIR}"
+cp "${CLONE_DIR}/notebooks/data/documentation_referentiel_arret.txt" "${WORK_DIR}"
+
+# Install packages
+pip install -r "${CLONE_DIR}/requirements.txt"
+
+# Remove useless repository
+rm -rf $CLONE_DIR
+
+# Open the relevant notebook when starting Jupyter Lab
+echo "c.LabApp.default_url = '/lab/tree/vector_db_template.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
